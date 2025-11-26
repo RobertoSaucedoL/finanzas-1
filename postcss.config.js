e93d9bca5b4a4a35cb738e2fec7e7b45329809f0
RobertoSaucedoL <zircksaucedo@gmail.com>
2025-11-25 21:54:56 -0600

    postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,3 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+// Archivo vacío intencionalmente para evitar conflictos con el CDN de Tailwind.
+// Al usar CDN, no necesitamos procesar CSS en el build.
+export default {};
