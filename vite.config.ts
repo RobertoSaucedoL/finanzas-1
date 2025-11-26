e961dc62cd3ae1c21e82ca209d66e432a982690a
RobertoSaucedoL <zircksaucedo@gmail.com>
2025-11-25 19:21:13 -0600

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // Vite expone automáticamente las variables que empiezan con VITE_
-  // No necesitamos configuración extra compleja.
 });
