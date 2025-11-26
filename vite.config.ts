580122b79b03b57e06336077766b9acc964cbfc9
RobertoSaucedoL <zircksaucedo@gmail.com>
2025-11-25 21:40:51 -0600

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// Configuración de Vite para React + TS
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
-    sourcemap: true
-  }
+    outDir: "dist",
+    sourcemap: true,
+  },
 });
